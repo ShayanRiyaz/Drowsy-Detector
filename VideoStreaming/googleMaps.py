790c2668d2356d04e9af453dfc763685a86cc570
karakuik <wesloke@gmail.com>
2020-09-26 22:38:23 -0400

    Revisions 10:38 Ryan

--- VideoStreaming/googleMaps.py
+++ VideoStreaming/googleMaps.py
@@ -3,16 +3,33 @@ import pprint
 
 API_KEY = 'AIzaSyBSDl8S2NWX5GwJHvLaQ2PMVJSSxur033Q'
 
-gmaps = googlemaps.Client(key = API_KEY)
+gmaps = googlemaps.Client(key=API_KEY)
 
-#Radius is in meters, curr lat+lon is ssrt to FIU, and only checks for open locations and gas stations
-places_result = gmaps.places_nearby(location='25.751496994, -80.37333184', radius = 4000, open_now = True, type = 'gas_station')
 
-#pprint.pprint(places_result)
+def googlemaps():
+    # Radius is in meters, curr lat+lon is ssrt to FIU, and only checks for open locations and gas stations
+    places_result = gmaps.places_nearby(location='25.751496994, -80.37333184', radius=4000, open_now=True,
+                                        type='gas_station')
 
-for place in places_result['results']:
-    place_id = place['place_id']
-    fields = ['name', 'formatted_phone_number', 'geometry/location', 'opening_hours']
-    place_details = gmaps.place(place_id = place_id, fields = fields)
+    # directions = gmaps.directions(origin= '9980 SW 104th St, Miami, FL 33176', destination = '1375 E Buena Vista
+    # Dr, Orlando, FL 32830', mode = "driving")
 
-    print(place_details)
+    # This gets the places near the Current location
+    places = gmaps.places(query="Rest Stops", location='25.751496994, -80.37333184', radius=4000, open_now=True)
+    #pprint.pprint(places)
+    for place in places['results']:
+        place_id = place['place_id']
+        fields = ['name', 'formatted_phone_number', 'geometry/location']
+        place_details = gmaps.place(place_id=place_id, fields=fields)
+        print(place_details)
+
+    print('\n\n\n\n\n')
+
+    for place in places_result['results']:
+        place_id = place['place_id']
+        fields = ['name', 'formatted_phone_number', 'geometry/location']
+        place_details = gmaps.place(place_id=place_id, fields=fields)
+        print(place_details)
+
+
+googlemaps()
